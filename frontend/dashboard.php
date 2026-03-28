60aef8b6c53ef3d05bb9f753b409f0946436f4b1
ITheMythicI <erick.gabriel.perez@gmail.com>
2026-03-27 22:07:12 -0600

    Se añadió un extra para volver a la pagina principal

--- frontend/dashboard.php
+++ frontend/dashboard.php
@@ -30,11 +30,11 @@
         <div class="logo-area">
 
             <div class="logo-box">
-                <img src="assets/img/logo-nexus.png" alt="Nexus Solutions Logo">
+                <img src="BIN.png" alt="Nexus Solutions Logo">
             </div>
 
             <div>
-                <h1>Nexus Solutions</h1>
+                <h1>Portal BIN</h1>
                 <p>Bienvenido</p>
             </div>
 
@@ -71,7 +71,13 @@
         <div class="nav-section">GENERAL</div>
 
         <div class="menu-item">
-            <a href="Dashboard.php" class="menu-btn active">
+            <a href="index.html" class="menu-btn active">
+                <span><i class="fa-solid fa-gauge-high"></i> Página Principal </span>
+            </a>
+        </div>
+
+        <div class="menu-item">
+            <a href="dashboard.php" class="menu-btn active">
                 <span><i class="fa-solid fa-gauge-high"></i> Dashboard</span>
             </a>
         </div>
