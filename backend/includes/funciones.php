3651987a9d8bb67ea33a43b6f915f952faefa928
PepeBeto309 <carrillo.jose.dev@gmail.com>
2026-03-27 21:15:19 -0600

    Correccion nombre de tabla de la base de datos

--- backend/includes/funciones.php
+++ backend/includes/funciones.php
@@ -3,7 +3,7 @@
 function obtener_tabla(){
     try {
         require __DIR__ . '/database.php';
-        $consulta = "SELECT * FROM contenedores;";
+        $consulta = "SELECT * FROM Contenedores;";
         return $resultado = mysqli_query($db, $consulta);
     } catch (\Throwable $th) {
         var_dump($th);
