1bb770bea23d992eb4290034517a71aa964d0b48
ITheMythicI <erick.gabriel.perez@gmail.com>
2026-04-24 21:07:30 -0600

    feat: Se rediseñó la sección del mapa para ser más atractiva visualmente, se le añadieron algunas características que se tenían planeadas en un inicio

--- frontend/dashboardMapa.php
+++ frontend/dashboardMapa.php
@@ -298,17 +298,6 @@ L.marker([25.533882815132063, -103.4352017462659], {icon: iconQuimica}).addTo(ma
 
     </main>
 
-    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-            integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="                
-            crossorigin="">
-    </script>
-
-    <script>
-        const datosContenedores = <?php echo json_encode($datos_contenedores); ?>;
-    </script>
-
-    <script src="js/mapaContenedores.js"></script>
-
 </body>
 
 </html>
