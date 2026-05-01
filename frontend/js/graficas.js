2098002af2d539762b12b4226ae7eff92572131d
jockabed <257610341+jockabed@users.noreply.github.com>
2026-04-30 20:20:17 -0600

    feat: Botón de registrar ya dirige a dashboard, prueba de gráficas

---
+++ frontend/js/graficas.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const barras = document.getElementById('tabla_barras');
+
+  new Chart(barras, {
+    type: 'bar',
+    data: {
+      labels: ['1', '2', '3', '4', '5', '6'],
+      datasets: [{
+        label: 'PORCENTAJE',
+        data: [95, 10, 55, 75, 85, 60],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+  new Chart(document.getElementById('tabla_dona'), {
+
+    type: 'doughnut',
+    data: {
+        labels: ['150L', '200L', '300L'],
+        datasets: [{
+            label: 'My First Dataset',
+        data: [2, 4, 1],
+        backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)'
+        ],
+        hoverOffset: 4
+        }]
+    },
+     options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+    
+});
+
+});
+
